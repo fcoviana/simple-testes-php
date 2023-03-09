6f5e565630e077f35c4d9f14ff2cc3d6c286e8a7
Francisco Viana <franciscoviana862@gmail.com>
2023-03-09 08:35:41 -0300

    refactor cart

--- src/Cart.php
+++ src/Cart.php
@@ -2,29 +2,17 @@
 
 namespace App;
 
-// use \App\Items;
-// use \App\Item;
-
-// use App\Exption\UnexpectedNonNumericExpetion;
-
 class Cart
 {
 
     function getTotal(Items $itens)
-    {   
+    {
         $total = 0;
-        for ($i = 0; $i < $itens->count(); $i++) { 
+        for ($i = 0; $i < $itens->count(); $i++) {
             $item = $itens->current();
             $total += $item->getValue();
             $itens->next();
         }
-        // foreach ($itens as $item) {
-        //     // if (!is_numeric($item)) {
-        //     //     throw new UnexpectedNonNumericExpetion($item);
-        //     // }
-        //     $total += $item->getValue();
-        // }
-        
         return $total;
     }
 }
